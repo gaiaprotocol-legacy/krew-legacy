5c35ea561bb330ed2c7efb3be724f7454db62924
yjgaia <hanul@hanul.me>
2023-11-20 15:52:18 +0900

    users_public

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -24,10 +24,45 @@ CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
 
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+CREATE TABLE IF NOT EXISTS "public"."users_public" (
+    "user_id" "uuid" DEFAULT "auth"."uid"() NOT NULL,
+    "wallet_address" "text",
+    "total_earned_trading_fees" numeric DEFAULT '0'::numeric NOT NULL,
+    "display_name" "text",
+    "profile_image" "text",
+    "profile_image_thumbnail" "text",
+    "profile_image_stored" boolean DEFAULT false NOT NULL,
+    "x_username" "text",
+    "metadata" "jsonb",
+    "follower_count" integer DEFAULT 0 NOT NULL,
+    "following_count" integer DEFAULT 0 NOT NULL,
+    "blocked" boolean DEFAULT false NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "updated_at" timestamp with time zone
+);
+
+ALTER TABLE "public"."users_public" OWNER TO "postgres";
+
+ALTER TABLE ONLY "public"."users_public"
+    ADD CONSTRAINT "users_public_pkey" PRIMARY KEY ("user_id");
+
+ALTER TABLE ONLY "public"."users_public"
+    ADD CONSTRAINT "users_public_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id");
+
+ALTER TABLE "public"."users_public" ENABLE ROW LEVEL SECURITY;
+
 GRANT USAGE ON SCHEMA "public" TO "postgres";
 GRANT USAGE ON SCHEMA "public" TO "anon";
 GRANT USAGE ON SCHEMA "public" TO "authenticated";
 GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+GRANT ALL ON TABLE "public"."users_public" TO "anon";
+GRANT ALL ON TABLE "public"."users_public" TO "authenticated";
+GRANT ALL ON TABLE "public"."users_public" TO "service_role";
 
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
