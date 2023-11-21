dd6e99e3e8c783f8052bb66dd2ffe2b895e80594
yjgaia <hanul@hanul.me>
2023-11-21 18:16:56 +0900

    Design post components

---
+++ app/dev.ts
@@ -0,0 +1,7 @@
+import initialize from "./initialize.js";
+await initialize({
+  dev: true,
+  supabaseUrl: "https://sfwnwiuxgehxbyystchq.supabase.co",
+  supabaseAnonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmd253aXV4Z2VoeGJ5eXN0Y2hxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMTA5OTYsImV4cCI6MjAxNTc4Njk5Nn0.iQ9aIdYmqKOdeAXonb6LFy9DLSVMlWycxleueEIOZes",
+});
