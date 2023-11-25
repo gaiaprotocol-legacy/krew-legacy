36b193188376bebb8084a4b471abed46add95e48
yjgaia <hanul@hanul.me>
2023-11-25 22:49:45 +0900

    dev

---
+++ database/create-table/krews.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS "public"."krews" (
+    "id" "text" NOT NULL,
+    "last_fetched_key_price" numeric DEFAULT '10000000000000000'::numeric NOT NULL,
+    "total_trading_key_volume" numeric DEFAULT '0'::numeric NOT NULL,
+    "total_earned_trading_fees" numeric DEFAULT '0'::numeric NOT NULL,
+    "is_key_price_up" boolean,
+    "last_message" "text",
+    "last_message_sent_at" timestamp with time zone DEFAULT '-infinity'::timestamp with time zone NOT NULL,
+    "key_holder_count" integer DEFAULT 0 NOT NULL,
+    "last_key_purchased_at" timestamp with time zone DEFAULT '-infinity'::timestamp with time zone NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "updated_at" timestamp with time zone
+);
+
+ALTER TABLE "public"."krews" OWNER TO "postgres";
+
+ALTER TABLE ONLY "public"."krews"
+    ADD CONSTRAINT "krews_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE "public"."krews" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "view everyone" ON "public"."krews" FOR SELECT USING (true);
+
+GRANT ALL ON TABLE "public"."krews" TO "anon";
+GRANT ALL ON TABLE "public"."krews" TO "authenticated";
+GRANT ALL ON TABLE "public"."krews" TO "service_role";
