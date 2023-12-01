76bf033ebd1c426ad7afce1f3e58a11b47c04aee
yjgaia <hanul@hanul.me>
2023-12-01 21:57:36 +0900

    Create UserDisplay.ts

---
+++ app/user/UserDisplay.ts
@@ -0,0 +1,9 @@
+import { DomNode } from "common-app-module";
+
+export default class UserDisplay extends DomNode {
+  constructor(private xUsername: string) {
+    super(".user-display");
+  }
+
+  //TODO:
+}
