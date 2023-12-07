57481d3aaef12cb2f14faf76a1ff9a7fd3c54b61
yjgaia <hanul@hanul.me>
2023-12-08 00:01:06 +0900

    dev

--- app/dev.ts
+++ app/dev.ts
@@ -2,7 +2,7 @@ import initialize from "./initialize.js";
 await initialize({
   dev: true,
 
-  supabaseUrl: "https://sfwnwiuxgehxbyystchq.supabase.co",
+  /*supabaseUrl: "https://sfwnwiuxgehxbyystchq.supabase.co",
   supabaseAnonKey:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmd253aXV4Z2VoeGJ5eXN0Y2hxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMTA5OTYsImV4cCI6MjAxNTc4Njk5Nn0.iQ9aIdYmqKOdeAXonb6LFy9DLSVMlWycxleueEIOZes",
 
@@ -13,5 +13,19 @@ await initialize({
   kromaChainId: 2358,
 
   krewPersonalAddress: "0xECFFc91149b8B702dEa6905Ae304A9D36527060F",
-  krewCommunalAddress: "0xe741b5DF37FB86eaB58F616dA0f4BfF10251C37a",
+  krewCommunalAddress: "0xe741b5DF37FB86eaB58F616dA0f4BfF10251C37a",*/
+
+  // mainnet test
+  supabaseUrl: "https://nvpcdfjnookurpbeixkt.supabase.co",
+  supabaseAnonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52cGNkZmpub29rdXJwYmVpeGt0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5NTkyNTUsImV4cCI6MjAxNzUzNTI1NX0.55I5dRBCvMR1DoZ94Nf0PASTyLRURHWcP8-uGP1zlns",
+
+  walletConnectProjectId: "9ef2b6a581d3644cd09342a5f0ff5318",
+  messageForWalletLinking: "Link Wallet to Krew",
+
+  kromaRpc: "https://api.kroma.network",
+  kromaChainId: 255,
+
+  krewPersonalAddress: "0xECFFc91149b8B702dEa6905Ae304A9D36527060F",
+  krewCommunalAddress: "0x0",
 });
--- app/prod.ts
+++ app/prod.ts
@@ -2,9 +2,9 @@ import initialize from "./initialize.js";
 await initialize({
   dev: true,
 
-  supabaseUrl: "https://sfwnwiuxgehxbyystchq.supabase.co",
+  supabaseUrl: "https://nvpcdfjnookurpbeixkt.supabase.co",
   supabaseAnonKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmd253aXV4Z2VoeGJ5eXN0Y2hxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMTA5OTYsImV4cCI6MjAxNTc4Njk5Nn0.iQ9aIdYmqKOdeAXonb6LFy9DLSVMlWycxleueEIOZes",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52cGNkZmpub29rdXJwYmVpeGt0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5NTkyNTUsImV4cCI6MjAxNzUzNTI1NX0.55I5dRBCvMR1DoZ94Nf0PASTyLRURHWcP8-uGP1zlns",
 
   walletConnectProjectId: "9ef2b6a581d3644cd09342a5f0ff5318",
   messageForWalletLinking: "Link Wallet to Krew",
@@ -13,5 +13,5 @@ await initialize({
   kromaChainId: 255,
 
   krewPersonalAddress: "0xECFFc91149b8B702dEa6905Ae304A9D36527060F",
-  krewCommunalAddress: "",
+  krewCommunalAddress: "0x0",
 });
