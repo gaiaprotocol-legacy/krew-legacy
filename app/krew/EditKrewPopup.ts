1b2c9d6872453199fa04c84d7cd6343ae4d4d8f8
yjgaia <hanul@hanul.me>
2023-12-07 14:13:53 +0900

    dev

--- app/krew/EditKrewPopup.ts
+++ app/krew/EditKrewPopup.ts
@@ -1,13 +1,37 @@
-import { Component, Popup } from "common-app-module";
+import { Component, el, Popup } from "common-app-module";
 import Krew from "../database-interface/Krew.js";
 
 export default class EditKrewPopup extends Popup {
-  constructor(krew: string, previewKrew?: Krew) {
+  private krew: Krew | undefined;
+
+  constructor(private krewId: string, previewKrew?: Krew) {
+    super({ barrierDismissible: true });
+    this.krew = previewKrew;
+
     super({ barrierDismissible: true });
     this.append(
       new Component(
         ".popup.edit-krew-popup",
+        el(
+          "header",
+          el(".krew-image", {
+            style: {
+              backgroundImage: `url(${previewKrew?.image_thumbnail})`,
+            },
+          }),
+          el("h1", "Edit Krew"),
+        ),
+        el(
+          "main",
+        ),
+        el("footer"),
       ),
     );
+
+    this.fetchKrew();
+  }
+
+  private fetchKrew() {
+    //TODO:
   }
 }
--- app/krew/KrewPopup.ts
+++ app/krew/KrewPopup.ts
@@ -1,13 +1,71 @@
-import { Component, Popup } from "common-app-module";
+import { Component, el, Icon, Popup } from "common-app-module";
+import { ethers } from "ethers";
 import Krew from "../database-interface/Krew.js";
+import MaterialIcon from "../MaterialIcon.js";
+import EditKrewPopup from "./EditKrewPopup.js";
+import KrewUtil from "./KrewUtil.js";
 
 export default class KrewPopup extends Popup {
-  constructor(krew: string, previewKrew?: Krew) {
+  private krew: Krew | undefined;
+
+  constructor(private krewId: string, previewKrew?: Krew) {
     super({ barrierDismissible: true });
+    this.krew = previewKrew;
+
     this.append(
       new Component(
         ".popup.krew-popup",
+        el(
+          "header",
+          el(".krew-image", {
+            style: {
+              backgroundImage: `url(${previewKrew?.image_thumbnail})`,
+            },
+          }),
+          el("h1", previewKrew ? KrewUtil.getName(previewKrew) : undefined),
+          this.editButton = el("a.hidden", new Icon("edit"), {
+            click: () => new EditKrewPopup(krewId, this.krew),
+          }),
+        ),
+        el(
+          ".metric-container",
+          el(
+            "section.price",
+            el(".icon-container", new MaterialIcon("group")),
+            el(
+              ".metric",
+              el("h3", "Holders"),
+              el(
+                ".value",
+                previewKrew ? String(previewKrew.key_holder_count) : "...",
+              ),
+            ),
+          ),
+          el(
+            "section.price",
+            el(".icon-container", new MaterialIcon("sell")),
+            el(
+              ".metric",
+              el("h3", "Price"),
+              el(
+                ".value",
+                previewKrew
+                  ? ethers.formatEther(previewKrew.last_fetched_key_price) +
+                    " ETH"
+                  : "...",
+              ),
+            ),
+          ),
+        ),
+        el(".balance", "..."),
+        el("footer"),
       ),
     );
+
+    this.fetchKrew();
+  }
+
+  private fetchKrew() {
+    //TODO:
   }
 }
