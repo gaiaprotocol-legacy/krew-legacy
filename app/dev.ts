b5f8513961ebaeb0c215ff37e1f5a487c1418572
yjgaia <hanul@hanul.me>
2023-12-11 13:46:45 +0900

    dev

--- app/dev.ts
+++ app/dev.ts
@@ -2,18 +2,15 @@ import initialize from "./initialize.js";
 await initialize({
   dev: true,
 
-  supabaseUrl: "https://sfwnwiuxgehxbyystchq.supabase.co",
+  supabaseUrl: "http://localhost:54321",
   supabaseAnonKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmd253aXV4Z2VoeGJ5eXN0Y2hxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMTA5OTYsImV4cCI6MjAxNTc4Njk5Nn0.iQ9aIdYmqKOdeAXonb6LFy9DLSVMlWycxleueEIOZes",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
 
   walletConnectProjectId: "9ef2b6a581d3644cd09342a5f0ff5318",
   messageForWalletLinking: "Link Wallet to Krew",
 
-  //kromaRpc: "https://api.sepolia.kroma.network",
-  //kromaChainId: 2358,
-
-  kromaRpc: "https://api.kroma.network",
-  kromaChainId: 255,
+  kromaRpc: "https://api.sepolia.kroma.network",
+  kromaChainId: 2358,
 
   krewPersonalAddress: "0xECFFc91149b8B702dEa6905Ae304A9D36527060F",
   krewCommunalAddress: "0x0",
